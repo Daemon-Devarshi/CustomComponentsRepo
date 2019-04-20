3b4599b1322feba93f3850d522b3220b4da08f00
Devarshi <devarshi>
2019-04-21 05:27:41 +0530

    Initial commit

---
+++ CustomComponents/CustomComponents.h
@@ -0,0 +1,19 @@
+//
+//  CustomComponents.h
+//  CustomComponents
+//
+//  Created by Devarshi on 4/22/18.
+//  Copyright © 2018 D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomComponents.
+FOUNDATION_EXPORT double CustomComponentsVersionNumber;
+
+//! Project version string for CustomComponents.
+FOUNDATION_EXPORT const unsigned char CustomComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomComponents/PublicHeader.h>
+
+
